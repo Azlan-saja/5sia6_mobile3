019d63bd101e0f36f9ea9ade02ed1a0faeac4427
Azlan-saja <azlansaja19@gmail.com>
2025-09-18 11:08:38 +0700

    p10

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi_5sia6_mobile3/views/login_view.dart';
 import 'package:flutter/material.dart';
 
 class NoteView extends StatelessWidget {
@@ -5,6 +6,44 @@ class NoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        title: Text('Notes App'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginView(),
+                  ),
+                  (route) => false,
+                );
+              },
+              icon: Icon(Icons.logout)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: TextFormField(
+          decoration: InputDecoration(
+            hintText: "Search Note",
+            prefixIcon: Icon(Icons.search),
+            border: InputBorder.none,
+            fillColor: Colors.grey.withAlpha(40),
+            filled: true,
+            contentPadding: EdgeInsets.only(top: 13),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
